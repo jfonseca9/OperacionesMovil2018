f8dae4c10747cbbae83c279d7042346716054a86
Jaime <j.efonsecaguerrero@gmail.com>
2018-02-26 19:53:00 -0500

    Interfaz terminada

---
+++ app/src/main/java/com/holamundo/operaciones/OperacionesMovil2018.java
@@ -0,0 +1,13 @@
+package com.holamundo.operaciones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OperacionesMovil2018 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operaciones_movil2018);
+    }
+}
