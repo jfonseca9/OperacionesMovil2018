d7cad3074b954cd800a3c93408dd11bf71d741f2
Jaime <j.efonsecaguerrero@gmail.com>
2018-02-26 20:27:30 -0500

    Funcionalidad Suma sin validaciones

---
+++ app/src/main/java/com/holamundo/operaciones/Metodos.java
@@ -0,0 +1,12 @@
+package com.holamundo.operaciones;
+
+/**
+ * Created by android on 26/02/2018.
+ */
+
+public class Metodos {
+
+    public static double sumar (double n1, double n2){
+        return n1+n2;
+    }
+}
